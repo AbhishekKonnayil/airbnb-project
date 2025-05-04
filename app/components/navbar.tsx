63a78173f7e6b73f595127411bd83765d40b9e70
Abhishek <abhishek.konnayil@gmail.com>
2025-05-04 10:41:45 +0530

    initial commit

---
+++ app/components/navbar.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import LogoImage from "../../public/logo.png";
+import SearchFilters from "./searchfilters";
+
+const Navbar = () => {
+  return (
+    <div className="w-full top-0 left-0 fixed bg-white border-b z-10 py-6">
+      <div className="max-w-[1500px] mx-auto px-6">
+        <div className="flex justify-between items-center">
+          <Link href="/">
+            <Image src={LogoImage} alt="Logo Image" width={180} height={38} />
+          </Link>
+          <div className="flex space-x-6">
+            <SearchFilters />
+          </div>
+          <div className="flex items-center space-x-6">Nav menu</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
