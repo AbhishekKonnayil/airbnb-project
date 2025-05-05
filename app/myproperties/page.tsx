c06e63aa4c5cc52ac57d1e357458a7718561fe6c
Abhishek <abhishek.konnayil@gmail.com>
2025-05-05 06:08:11 +0530

    feat:inbox page creating

---
+++ app/myproperties/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropertiesList from "../components/Properties/PropertiesList";
+
+const MyProperties = () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6 ">
+      <h1 className="text-2xl my-6"> My Properties</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <PropertiesList />
+      </div>
+    </main>
+  );
+};
+
+export default MyProperties;
