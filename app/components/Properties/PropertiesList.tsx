f43bad8ad2db7f1320bf8a054995a6823c46151b
Abhishek <abhishek.konnayil@gmail.com>
2025-05-09 06:52:11 +0530

    connected backend

--- app/components/Properties/PropertiesList.tsx
+++ app/components/Properties/PropertiesList.tsx
@@ -1,13 +1,43 @@
-import React from "react";
+"use client";
+import { useEffect, useState } from "react";
 import PropertiesListItems from "./PropertiesListItems";
+import { json } from "stream/consumers";
+import { error } from "console";
+export type PropertyType = {
+  title: string;
+  id: string;
+  price_per_night: string;
+  image_url: string;
+};
 
 const PropertiesList = () => {
+  const [properties, setProperties] = useState<PropertyType[]>([]);
+  const getProperties = async () => {
+    const url = "http://localhost:8000/api/properties/";
+
+    await fetch(url, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("json", json);
+
+        setProperties(json.data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+
+  useEffect(() => {
+    getProperties();
+  }, []);
+
   return (
     <>
-      <PropertiesListItems />
-      <PropertiesListItems />
-      <PropertiesListItems />
-      <PropertiesListItems />
+      {properties.map((property) => {
+        return <PropertiesListItems key={property.id} property={property} />;
+      })}
     </>
   );
 };
--- app/components/Properties/PropertiesListItems.tsx
+++ app/components/Properties/PropertiesListItems.tsx
@@ -1,24 +1,31 @@
 import Image from "next/image";
 import React from "react";
 import BeachImage from "../../../public/cabin-image1.avif";
+import { PropertyType } from "./PropertiesList";
 
-const PropertiesListItems = () => {
+interface PropertyProps {
+  property: PropertyType;
+}
+
+const PropertiesListItems:React.FC<PropertyProps> = ({property}) => {
   return (
     <div className="cursor-pointer">
       <div className="relative overflow-hidden aspect-square rounded-xl">
         <Image
           fill
-          src={BeachImage}
+          src={property.image_url}
           alt="cabin-image1"
           sizes="(max-width:768px)768px,(max-width:1200px),768px,768px"
           className="hover:scale-110 object-cover transition h-full w-full"
         />
       </div>
       <div className="mt-2">
-        <p className="text-lg font-bold">Property Name</p>
+        <p className="text-lg font-bold">{property.title}</p>
       </div>
       <div className="mt-2">
-        <p className="text-sm text-gray-500"><strong>$200</strong> for 5 nights</p>
+        <p className="text-sm text-gray-500">
+          <strong>{property.price_per_night}</strong> for 5 nights
+        </p>
       </div>
     </div>
   );
