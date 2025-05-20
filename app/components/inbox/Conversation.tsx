e9d50394d4a397a70640c6d5f1077cc2588e4e46
Abhishek <abhishek.konnayil@gmail.com>
2025-05-21 00:26:48 +0530

    setting up conversation feature

--- app/components/inbox/Conversation.tsx
+++ app/components/inbox/Conversation.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+"use client";
 
-const conversation = () => {
+import { ConversationType } from "@/app/inbox/page";
+import { useRouter } from "next/navigation";
+
+interface ConversationProps {
+  userId: string;
+  conversation: ConversationType;
+}
+const conversation: React.FC<ConversationProps> = ({
+  userId,
+  conversation,
+}) => {
+  const router = useRouter();
+  const otherUser = conversation.users.find((user) => user.id !== userId);
+  console.log("All users:", conversation.users);
+  console.log("Current userId:", userId);
+  console.log("Found otherUser:", otherUser);
   return (
-      <div className="border border-gray-300 py-4 px-6 rounded-xl cursor-pointer">
-        <p className="text-xl mb-6">Abhishek</p>
-        <p className="text-airbnb-dark">Go to Conversation</p>
-      </div>
+    <div className="border border-gray-300 py-4 px-6 rounded-xl cursor-pointer">
+      <p className="text-xl mb-6">{otherUser?.name}</p>
+
+      <p
+        onClick={() => router.push(`/inbox/${conversation.id}`)}
+        className="text-airbnb-dark"
+      >
+        Go to Conversation
+      </p>
+    </div>
   );
 };
 
--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -1,16 +1,40 @@
 import React from "react";
 import Conversation from "../components/inbox/Conversation";
+import { getUserId } from "../lib/Action";
+import apiSevice from "../apiSevice";
 
-const Inbox = () => {
+export type UserType = {
+  id: string;
+  name: string;
+  avatar_url: string;
+};
+
+export type ConversationType = {
+  id: string;
+  users: UserType[];
+};
+
+const InboxPage = async () => {
+  const userId = await getUserId();
+  if (!userId) {
+    <h1 className="text-2xl my-6"> You need to be authenticated </h1>;
+  }
+
+  const Conversations = await apiSevice.get("/api/chat/");
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4 ">
       <h1 className="text-2xl my-6">Inbox</h1>
 
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
+      {Conversations?.map((conversation: ConversationType) => {
+        return (
+          <Conversation
+            key={conversation.id}
+            userId={userId ?? ""}
+            conversation={conversation}
+          />
+        );
+      })}
     </main>
   );
 };
-
-export default Inbox;
+export default InboxPage;
